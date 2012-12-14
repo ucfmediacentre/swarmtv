c8d268113c50da187bd86fa0e939038caff25cdc
Jem Mackay <jem@swarmtv.org>
2012-12-14 07:43:27 +0000

    Got a page to collect all content to do do with a page and print it down the screen

--- codeigniter/application/controllers/pages.php
+++ codeigniter/application/controllers/pages.php
@@ -22,23 +22,12 @@ class Pages extends CI_Controller {
 		$page_details= $this->Pages_model->get_page($page_title);
 		$data['page_info'] = $page_details;
 		
-		/*if($page_details) 
+		if($page_details) 
 		{
 			// get the page contents
 			$this->load->model('Contents_model');
 			$page_contents= $this->Contents_model->get_all_contents($page_details->id);
-			
-			$listview = '';
-    		$temp_x =200;
-    		$temp_y = 100;
-			foreach ($page_contents->result() as $row)
-			{
-    			$listview = $listview . '<div id="div' . $row->id . 'type="text" class="element" style="position: absolute; opacity: 1; color: rgb(192, 192, 192); background: url(&quot;transparentpixel.gif&quot;) repeat scroll 0% 0% transparent; font-size: 15px; width: 554px; height: 188px; left: ' . $temp+x . 'px; top: ' . $temp_y . 'px; z-index: 5437; overflow: auto; margin: 1px; padding: 15px; cursor: auto; border: medium none;" onmouseover="this.style.border=\'1px dashed #888\'; this.style.margin=\'0px\';" onmouseout="this.style.border=\'none\'; this.style.margin=\'1px\';" ondblclick="openEditor(\'div' . $row->id . ');">' . $row->content . '</div>';
-    			$temp_y = $temp_y+200;
-			}
-    		
-			$data['page_contents'] = $listview;*/
-			//$data['page_contents'] = $listview;
+			$data['page_contents'] = $page_contents;
 			
 			// load view with data
 			$this->load->view('header');
--- codeigniter/application/views/page_view.php
+++ codeigniter/application/views/page_view.php
@@ -2,9 +2,18 @@
 <p> <?php echo $page_info->description; ?> </p>
 
 <?php 
-foreach ($page_contents->result() as $content)
-{
-	echo $content->description . '<br />';
-} 
+//foreach ($data['page_info']->result() as $content)
+//{
+	//echo $content->contents . '<br />';
+	$listview = '';
+	$temp_x =200;
+	$temp_y = 100;
+	foreach ($page_contents->result() as $row)
+	{
+		$listview = $listview . '<div id="div' . $row->id . 'type="text" class="element" style="position: absolute; opacity: 1; color: rgb(192, 192, 192); background: url(&quot;transparentpixel.gif&quot;) repeat scroll 0% 0% transparent; font-size: 15px; width: 554px; height: 188px; left: ' . $temp_x . 'px; top: ' . $temp_y . 'px; z-index: 5437; overflow: auto; margin: 1px; padding: 15px; cursor: auto; border: medium none;" onmouseover="this.style.border=\'1px dashed #888\'; this.style.margin=\'0px\';" onmouseout="this.style.border=\'none\'; this.style.margin=\'1px\';" ondblclick="openEditor(\'div' . $row->id . ');">' . $row->contents . '</div>';
+		$temp_y = $temp_y+200;
+	}
+	echo $listview;
+//} 
 
 ?>
