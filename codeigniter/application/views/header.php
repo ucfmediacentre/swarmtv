a502f22badaeda77e9bd1cc59fb2ee4881b7181a
Jem Mackay <jem@swarmtv.org>
2012-12-18 21:39:05 +0000

    makes text and images draggable and resizable

--- codeigniter/application/views/header.php
+++ codeigniter/application/views/header.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" href="<?php echo base_url(); ?>css/main.css">
         <link rel="stylesheet" href="<?php echo base_url(); ?>libraries/fancybox/jquery.fancybox-1.3.4.css" type="text/css" media="screen" />
         <link rel="stylesheet" href="<?php echo base_url(); ?>libraries/fineuploader.jquery-3.0/fineuploader.css" type="text/css" media="screen" />
-        <link rel="stylesheet" href="<?php echo base_url(); ?>libraries/jquery-ui-1.9.2.custom/css/egglant/jquery-ui-1.9.2.custom.min.css" type="text/css" media="screen" />
+        <link rel="stylesheet" href="<?php echo base_url(); ?>libraries/jquery-ui-1.9.2.custom/css/eggplant/jquery-ui-1.9.2.custom.min.css" type="text/css" media="screen" />
         
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src="<?php echo base_url(); ?>js/vendor/modernizr-2.6.2.min.js"></script>
