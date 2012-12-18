54ef88086a5c4ff52544efe7381496f0aca11cc7
Jem Mackay <jem@swarmtv.org>
2012-12-18 06:31:30 +0000

    creates elements on the page (only text and images so far)

--- codeigniter/application/views/pages_view/page_view.php
+++ codeigniter/application/views/pages_view/page_view.php
@@ -4,12 +4,27 @@
 
 //Create a div for each piece of text on the page as old SwarmTV used to do;
 $listview = '';
-$temp_x =200;
-$temp_y = 100;
 foreach ($page_contents->result() as $row)
 {
-	$listview = $listview . '<div id="' . $row->id . '" type="text" class="element" style="position: absolute; opacity: 1; color: rgb(192, 192, 192); background: url(&quot;transparentpixel.gif&quot;) repeat scroll 0% 0% transparent; font-size: 15px; width: 554px; height: 188px; left: ' . $temp_x . 'px; top: ' . $temp_y . 'px; z-index: 5437; overflow: auto; margin: 1px; padding: 15px; cursor: auto; border: medium none;" onmouseover="this.style.border=\'1px dashed #888\'; this.style.margin=\'0px\';" onmouseout="this.style.border=\'none\'; this.style.margin=\'1px\';" ondblclick="openEditor(\'div' . $row->id . ');">' . $row->content . '</div>';
-	$temp_y = $temp_y+200;
+	$elementType = $row->type;
+	
+	switch ($elementType) {
+    case "text":
+        $listview = $listview . '<div id="' . $row->id . '" type="' . $elementType . '" class="element" style="position: absolute; opacity: ' . $row->opacity . '; ' . $row->color . '; background: url(&quot;transparentpixel.gif&quot;) repeat scroll 0% 0% ' . $row->backgroundColor . '; font-family: ' . $row->fontFamily . '; font-size: ' . $row->fontSize . 'px; text-align: ' . $row->textAlign . '; width: ' . $row->width . 'px; height: ' . $row->height . 'px; left: ' . $row->x . 'px; top: ' . $row->y . 'px; z-index: ' . $row->z . '; overflow: auto; margin: 1px; padding: 15px; cursor: auto; border: medium none;" onmouseover="this.style.border=\'1px dashed #888\'; this.style.margin=\'0px\';" onmouseout="this.style.border=\'none\'; this.style.margin=\'1px\';" ondblclick="openEditor(\'div' . $row->id . ');">' . $row->content . '</div>';
+        break;
+    case "image":
+        
+    	$listview = $listview . '<div id="' . $row->id . '" type="' . $elementType . '" class="element" style="position: absolute; opacity: ' . $row->opacity . '; width: ' . $row->width . 'px; height: ' . $row->height . 'px; left: ' . $row->x . 'px; top: ' . $row->y . 'px; z-index: ' . $row->z . '; margin: 1px; padding: 15px; cursor: auto; border: medium none;" onmouseover="this.style.border=\'1px dashed #888\'; this.style.margin=\'0px\';" onmouseout="this.style.border=\'none\'; this.style.margin=\'1px\';" ondblclick="openEditor(\'div' . $row->id . ');"><img width="' . $row->width . '" height="' . $row->height . '" alt="' . $row->description . '" style="z-index:' . ($row->z-1) . ';" name="' . $row->filename . '" src="../../../../public/img/' . $row->filename . '"></div>';
+
+        break;
+    case "audio":
+        echo "stuff";
+        break;
+    case "movie":
+        echo "stuff";
+        break;
+	}
+	
 }
 echo $listview;
 ?>
